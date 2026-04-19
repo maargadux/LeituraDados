a98393976455f937f3e195cb8e7fab1f3a50683c
Mali <144566481+maargadux@users.noreply.github.com>
2026-04-19 17:08:42 -0300

    refatorando algumas informacoes
    cod aberto 1.2 - com comments

--- LeituraDados.java
+++ LeituraDados.java
@@ -1,33 +1,30 @@
 package com.java.leituradados;
 import java.util.Scanner;
-
-///CTRL + SPACE == Automatic Space yeeaaasss
-
+/// CTRL + SPACE = Automatic Imports YEAAAAAAAAAs
 public class LeituraDados {
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-	///obs: nome ficticio
-        System.out.println("Biqueira ZS");
-		
-            System.out.println("Opcoes disponiveis:");
-            System.out.println("1 - Prensado");
-            System.out.println("2 - Skunk");
-            System.out.println("3 - Dry");
-            System.out.println("Qual voce deseja?");
 
-            int escolha = sc.nextInt();
-		///pede uma resposta - numero.
-            if (escolha == 1) {
-                System.out.println("tu escolheu o prensado, toma! boa brisa.");
-            } else if (escolha == 2) {
-                System.out.println("tu escolheu o skunk, toma! boa brisa.");
-            } else if (escolha == 3) {
-                System.out.println("tu escolheu o dry, toma! boa brisa.");
-            } else {
-                System.out.println("opcao invalida");
-            }
+        System.out.println("Biqueira ZS");
+			///obs: nome ficticio
+        System.out.println("Opcoes disponiveis:");
+        System.out.println("1 - Prensado");
+        System.out.println("2 - Skunk");
+        System.out.println("3 - Dry");
+        System.out.println("Qual voce deseja?");
+
+        int escolhas = sc.nextInt();
+			///pede uma resposta - numero.
+        if (escolhas == 1) {
+            System.out.println("tu escolheu o prensado, toma! boa brisa.");
+        } else if (escolhas == 2) {
+            System.out.println("tu escolheu o skunk, toma! boa brisa camarada.");
+        } else if (escolhas == 3){
+            System.out.println("tu escolheu o dry, toma! boa brisa man");
+        } else {
+            System.out.println("opcao invalida");
         }
     }
 }
